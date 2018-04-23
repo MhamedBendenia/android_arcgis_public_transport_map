2203b2b90a0f3f099c4fff7fdc485b3025c3e1d1
MhamedBendenia <medbendenia27@gmail.com>
2018-04-24 01:14:55 +0200

    fond de carte ESRI

--- ETUM/app/src/main/java/com/pfe/musmed/etum/MainActivity.java
+++ ETUM/app/src/main/java/com/pfe/musmed/etum/MainActivity.java
@@ -2,6 +2,8 @@ package com.pfe.musmed.etum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
 
 import com.esri.arcgisruntime.mapping.view.MapView;
 
@@ -11,11 +13,32 @@ public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView;
 
+    @Override
+    protected void onPause(){
+        mMapView.pause();
+        super.onPause();
+    }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.dispose();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 35.931488, 0.092265, 16);
+        mMapView.setMap(map);
     }
 }
+
