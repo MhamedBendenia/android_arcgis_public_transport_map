13700871ea8d061257f954443e97760aa6b00bdb
MhamedBendenia <medbendenia27@gmail.com>
2018-04-28 12:10:35 +0200

    Chargement de la base effectué.

--- ETUM/app/src/main/java/com/pfe/musmed/etum/MainActivity.java
+++ ETUM/app/src/main/java/com/pfe/musmed/etum/MainActivity.java
@@ -2,27 +2,34 @@ package com.pfe.musmed.etum;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.esri.arcgisruntime.data.FeatureTable;
+import com.esri.arcgisruntime.data.GeoPackage;
+import com.esri.arcgisruntime.data.Geodatabase;
+import com.esri.arcgisruntime.data.GeodatabaseFeatureTable;
+import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
-
-
-
-
-
 
 public class MainActivity extends AppCompatActivity {
     String[] reqPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission
             .ACCESS_COARSE_LOCATION};
+    private final String TAG = MainActivity.class.getSimpleName();
     private int requestCode = 2;
     private MapView mMapView;
+    private ArcGISMap mMap;
     private LocationDisplay mLocationDisplay;
 
     @Override
@@ -98,10 +105,60 @@ public class MainActivity extends AppCompatActivity {
                     if (!mLocationDisplay.isStarted())
                         mLocationDisplay.startAsync();
                 }
-                            }
+            }
         });
         mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.NAVIGATION);
         if (!mLocationDisplay.isStarted())
             mLocationDisplay.startAsync();
     }
+
+    private void loadGeodatabase() {
+
+        // create path to local geodatabase
+        String path =
+                Environment.getExternalStorageDirectory() + getString(R.string.config_data_sdcard_offline_dir)
+                        + getString(R.string.config_geodb_name);
+
+        // create a new geodatabase from local path
+        final Geodatabase geodatabase = new Geodatabase(path);
+
+        // load the geodatabase
+        geodatabase.loadAsync();
+
+        // create feature layer from geodatabase and add to the map
+        geodatabase.addDoneLoadingListener(() -> {
+            if (geodatabase.getLoadStatus() == LoadStatus.LOADED) {
+                // access the geodatabase's feature table Trailheads
+                GeodatabaseFeatureTable geodatabaseFeatureTable = geodatabase.getGeodatabaseFeatureTable("default");
+                geodatabaseFeatureTable.loadAsync();
+                // create a layer from the geodatabase feature table and add to map
+                final FeatureLayer featureLayer = new FeatureLayer(geodatabaseFeatureTable);
+                featureLayer.addDoneLoadingListener(() -> {
+                    if (featureLayer.getLoadStatus() == LoadStatus.LOADED) {
+                        // set viewpoint to the feature layer's extent
+                        mMapView.setViewpointAsync(new Viewpoint(featureLayer.getFullExtent()));
+                    } else {
+                        Toast.makeText(MainActivity.this, "Feature Layer failed to load!", Toast.LENGTH_LONG).show();
+                        Log.e(TAG, "Feature Layer failed to load!");
+                    }
+                });
+                // add feature layer to the map
+                mMapView.getMap().getOperationalLayers().add(featureLayer);
+            } else {
+                Toast.makeText(MainActivity.this, "Geodatabase failed to load!", Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Geodatabase failed to load!");
+            }
+        });
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            loadGeodatabase();
+        } else {
+            // report to user that permission was denied
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.location_permission_denied),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
